292dd67b162d851499c5c5e7e64b010e61ba6eb5
j.fortin42 <j.fortin75017@gmail.com>
2015-11-16 04:27:53 +0100

    adding a third part of functions

---
+++ ft_strmap.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	while (s[i])
+	{
+		str[i] = f(s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
