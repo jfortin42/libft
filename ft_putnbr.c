d12106eeb63bbcd522d7990a446263e39140bd8e
j.fortin42 <j.fortin75017@gmail.com>
2015-12-09 15:24:44 +0100

    update putnbr

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -12,42 +12,32 @@
 
 #include "libft.h"
 
-static int	ft_nbrlen(int nbr)
-{
-	int	i;
-
-	i = 0;
-	if (nbr < 0)
-		return (-1);
-	while (nbr >= 10)
-	{
-		nbr = nbr / 10;
-		i++;
-	}
-	return (i);
-}
-
 void		ft_putnbr(int n)
 {
 	int	i;
 	int	j;
 	int	tmp;
+	int	signe;
 
-	if (n == -2147483648)
-		ft_putstr("-2147483648");
-	else if (n < 0)
+	i = 1;
+	signe = 1;
+	if (n < 0)
 	{
+		signe = -1;
 		ft_putchar('-');
-		n = -n;
 	}
-	i = ft_nbrlen(n);
-	while (i >= 0 && n != -2147483648)
+	tmp = n;
+	while (tmp < -9 || tmp > 9)
+	{
+		tmp = tmp / 10;
+		i++;
+	}
+	while (i-- >= 1)
 	{
 		j = 0;
 		tmp = n;
 		while (j++ < i)
 			tmp = tmp / 10;
-		ft_putchar(tmp % 10 + '0');
-		i--;
+		ft_putchar(signe * (tmp % 10) + '0');
 	}
 }
