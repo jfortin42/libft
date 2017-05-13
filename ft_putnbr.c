9aa58ac329874e8b7de0b9e8f8028d7a7da25fc4
jfortin42 <djo_68@hotmail.fr>
2017-05-13 17:52:20 +0200

    new ft_putnbr, easier and faster

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -3,40 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jfortin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jfortin <jfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 10:23:50 by jfortin           #+#    #+#             */
-/*   Updated: 2015/11/26 14:41:41 by jfortin          ###   ########.fr       */
+/*   Updated: 2017/05/13 17:49:23 by jfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_putnbr(int n)
+void	ft_putnbr(int nb)
 {
-	int	i;
-	int	j;
+	int	power;
 	int	tmp;
-	int	signe;
 
-	i = 1;
-	signe = 1;
-	if (n < 0)
-		signe = -1;
-	if (n < 0)
+	if ((power = nb < 0 ? -1 : 1) == -1)
 		ft_putchar('-');
-	tmp = n;
-	while (tmp < -9 || tmp > 9)
+	tmp = nb;
+	while (tmp /= 10)
+		power *= 10;
+	while (power)
 	{
-		tmp = tmp / 10;
-		i++;
-	}
-	while (i-- >= 1)
-	{
-		j = 0;
-		tmp = n;
-		while (j++ < i)
-			tmp = tmp / 10;
-		ft_putchar(signe * (tmp % 10) + '0');
+		ft_putchar((tmp = nb / power) + '0');
+		nb -= tmp * power;
+		power /= 10;
 	}
 }
