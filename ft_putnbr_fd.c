12f79f44e76a4d7726c35cf076e5f4dc3b60a229
jfortin42 <djo_68@hotmail.fr>
2017-05-13 18:13:46 +0200

    new ft_putnbr_fd, easier and faster

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -3,52 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jfortin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: jfortin <jfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 10:23:53 by jfortin           #+#    #+#             */
-/*   Updated: 2015/11/24 11:50:28 by jfortin          ###   ########.fr       */
+/*   Updated: 2017/05/13 18:12:19 by jfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_nbrlen(int nbr)
-{
-	int	i;
-
-	i = 0;
-	if (nbr < 0)
-		return (-1);
-	while (nbr >= 10)
-	{
-		nbr = nbr / 10;
-		i++;
-	}
-	return (i);
-}
-
 void		ft_putnbr_fd(int n, int fd)
 {
-	int		i;
-	int		j;
-	int		tmp;
+	int	power;
+	int	tmp;
 
-	if (n == -2147483648)
-		ft_putstr_fd("-2147483648", fd);
-	else if (n < 0)
-	{
+	if ((power = n < 0 ? -1 : 1) == -1)
 		ft_putchar_fd('-', fd);
-		n = -n;
-	}
 	tmp = n;
-	i = ft_nbrlen(n);
-	while (i >= 0 && n != -2147483648)
+	while (tmp /= 10)
+		power *= 10;
+	while (power)
 	{
-		tmp = n;
-		j = 0;
-		while (j++ < i)
-			tmp = tmp / 10;
-		ft_putchar_fd(tmp % 10 + '0', fd);
-		i--;
+		ft_putchar_fd((tmp = n / power) + '0', fd);
+		n -= tmp * power;
+		power /= 10;
 	}
 }
