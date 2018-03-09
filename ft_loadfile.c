44f5e6f2573d9e17c2ddc82f89c8f4d674e720f2
jfortin42 <djo_68@hotmail.fr>
2018-03-09 13:12:30 +0100

    add log error in ft_loadfile

--- ft_loadfile.c
+++ ft_loadfile.c
@@ -6,20 +6,32 @@
 /*   By: jfortin <jfortin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/09 17:55:25 by jfortin           #+#    #+#             */
-/*   Updated: 2017/11/14 16:30:53 by jfortin          ###   ########.fr       */
+/*   Updated: 2018/03/09 13:10:57 by jfortin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_loadfile(const char *path_file)
 {
 	int		fd;
 	char	*out;
 
+	out = NULL;
 	if ((fd = (open(path_file, O_RDONLY))) < 3)
+	{
+		ft_putstr_fd("error open ", 2);
+		ft_putendl_fd(path_file, 2);
+		perror(NULL);
 		return (NULL);
-	ft_readfile(&out, fd);
+	}
+	if (ft_readfile(&out, fd) == -1)
+	{
+		ft_putstr_fd("error read ", 2);
+		ft_putendl_fd(path_file, 2);
+		perror(NULL);
+	}
 	close(fd);
 	return (out);
 }
